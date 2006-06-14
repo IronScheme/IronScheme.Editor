df08c7393c9ec72f55ded5ad185b0d66785e16cd
leppie <leppie@d807f128-0b0f-0410-83aa-822195e668f3>
2006-06-14 17:58:55 +0000

    git-svn-id: http://svn.code.sf.net/p/xacc/code@1 d807f128-0b0f-0410-83aa-822195e668f3

---
+++ cs_lex/CAcceptAnchor.cs
@@ -0,0 +1,31 @@
+using System;
+
+namespace CS_Lex
+{
+  /// <summary>
+  /// Summary description for CAcceptAnchor.
+  /// </summary>
+  /***************************************************************
+  Class: CAcceptAnchor
+  **************************************************************/
+  class CAcceptAnchor
+  {
+    /***************************************************************
+          Member Variables
+          **************************************************************/
+    CAccept m_accept;
+    int m_anchor;
+
+    /***************************************************************
+          Function: CAcceptAnchor
+          **************************************************************/
+    CAcceptAnchor
+      (
+      )
+    {
+      m_accept = null;
+      m_anchor = CSpec.NONE;
+    }
+  }
+
+}
