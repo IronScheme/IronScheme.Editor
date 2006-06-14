cf292aac305552d74c7c4943a4f6ab2d507261ad
leppie <leppie@d807f128-0b0f-0410-83aa-822195e668f3>
2006-06-14 19:16:23 +0000

    git-svn-id: http://svn.code.sf.net/p/xacc/code@8 d807f128-0b0f-0410-83aa-822195e668f3

---
+++ xacc/Resources/Fake.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+
+namespace Xacc.Resources
+{
+	/// <summary>
+	/// Summary description for Fake.
+	/// </summary>
+	class Fake
+	{
+		public Fake()
+		{
+			//
+			// TODO: Add constructor logic here
+			//
+		}
+	}
+}
